08adb77a439346ad521b0973fe25e89ccb2fc625
Andrew Gorovoy <andrewgroovy@gmail.com>
2018-05-03 18:23:59 -0600

    jquery practice

---
+++ JQuerySelector/script.js
@@ -0,0 +1,6 @@
+$("div").css("backgroundColor","purple");
+$("div.highlight").css("width","200px");
+$("#third").css("border", "3px orange solid");
+var divs = $("div");
+
+divs[0].css("color", "pink");
