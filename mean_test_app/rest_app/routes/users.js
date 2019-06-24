2447b12d95f7b7f78855fe78870eba5b247e9df7
Andrew Gorovoy <andrewgroovy@gmail.com>
2019-06-24 14:52:24 -0600

    Completed basic profile databse using AJAX, and MEAN stack

---
+++ mean_test_app/rest_app/routes/users.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/userlist', function(req, res) {
+  // res.send('respond with a resource');
+  var db = req.db;
+  var collection = db.get('userlist');
+  collection.find({},{},function(e,docs){
+    res.json(docs);
+  });
+});
+
+
+router.post('/adduser', function(req,res) {
+  var db = req.db;
+  var collection = db.get('userlist');
+  collection.insert(req.body,function(err,result) {
+    res.send((err === null) ? {msg: ''} : {msg: err});
+  });
+});
+
+
+router.delete('/deleteuser/:id', function(req,res) {
+  var db = req.db;
+  var collection = db.get('userlist');
+  var userToDelete = req.params.id;
+  collection.remove({'_id':userToDelete},function(err) {
+    res.send((err===null) ? {msg: ''} : {msg: err});
+  });
+});
+
+module.exports = router;
+
+
